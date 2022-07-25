5f0fee17c9f14d86e65b15fb53d860d8c74237e4
Sergio Mulas Azabal <56138357+MrZabbah@users.noreply.github.com>
2022-07-25 17:51:05 +0200

    [HOTFIX] Deleted unused constants

--- MyTracker/app/src/main/java/com/mrzabbah/mytracker/feature_book_tracker/data/data_remote/util/HttpRoutes.kt
+++ MyTracker/app/src/main/java/com/mrzabbah/mytracker/feature_book_tracker/data/data_remote/util/HttpRoutes.kt
@@ -4,7 +4,4 @@ object HttpRoutes {
 
     private const val BASE_URL = "https://www.googleapis.com"
     const val VOLUMES = "$BASE_URL/books/v1/volumes"
-    const val GOOGLE_BOOKS_API_KEY = "AIzaSyDV9fRubItcahlBaWUEE39rtDNmqot5f_4"
-    const val GOOGLE_BOOKS_API_KEY2 = "AIzaSyCt--_KQPud07EvrNYf_rqWELPaOOforJg"
-    const val TEST = "$VOLUMES?q=mistborn&inauthor=brandom+sanderson&maxResults=40&fields=items(volumeInfo(title))"
 }
